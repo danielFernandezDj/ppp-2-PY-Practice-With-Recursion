9dff3667ddaf31504cb040682c4e479cf11fb8d8
Daniel Fernandez <mdanif9728@gmail.com>
2024-08-17 11:18:02 -0700

    complete the solution1

--- solution1.py
+++ solution1.py
@@ -1,1 +1,28 @@
 # Write code for algorithm 1 below
+
+# 1. Write a program that takes a positive number as an argument and prints the numbers from n to zero.
+def positive_num(num):
+
+  # base case
+  if num == 1:
+    return 1
+
+  # recursive case
+  else:
+    return (num * positive_num(num - 1))
+
+print(positive_num(4))
+
+
+# Other example:
+def count_down(n):
+  #inherent base case
+  #(will stop if n <= 0)
+  if n>0:
+      #recursive case
+      print(n)
+      count_down(n-1)
+
+#test case
+n=8
+count_down(n)
