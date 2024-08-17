84a8b9d08a25af1f61da858a442f5f3e7316b49a
Daniel Fernandez <mdanif9728@gmail.com>
2024-08-17 11:26:40 -0700

    complete the solution 2

--- solution2.py
+++ solution2.py
@@ -1,1 +1,10 @@
 # Write code for algorithm 2 below
+
+# 2. Write a function that prints the natural numbers from 1 to n.
+def natural_numbers(n, i=1):
+
+  if i <= n:
+    print(i)
+    natural_numbers(n, i + 1)
+
+natural_numbers(3)
